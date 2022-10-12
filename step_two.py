620b8834fd007f4e7919cadddca3288d7f303f20
cockmake <81823756+cockmake@users.noreply.github.com>
2022-10-12 21:25:13 +0800

    Update step_two.py

--- step_two.py
+++ step_two.py
@@ -1,4 +1,7 @@
-from DataType import DataType
+import threading
+import time
+
+from DataType import DataType, SendFrame
 import tensorrt as trt
 import torch
 from collections import OrderedDict, namedtuple
@@ -7,38 +10,23 @@ from flask_cors import CORS
 import base64
 import cv2 as cv
 import numpy as np
-from collections import deque
-
+import os
+from utils import base64_to_image
 app = Flask(__name__)
 CORS(app)
 
 # 模型部分
 confidence = 0.5
-nms_threshold = 0.3
+nms_threshold = 0.4
 input_shape = (1, 3, 640, 640)
 N, C, H, W = input_shape
 logger = trt.Logger(trt.Logger.INFO)
 device = torch.device('cuda:0')
 Binding = namedtuple('Binding', ('name', 'dtype', 'shape', 'data', 'ptr'))
-bindings = OrderedDict()
-bindings_addrs = OrderedDict()
+bindings, bindings2 = OrderedDict(), OrderedDict()
+bindings_addrs, bindings_addrs2 = OrderedDict(), OrderedDict()
 context, context2 = None, None
-
-
-d_queue = deque(maxlen=100)
-
-# 工具函数
-def base64_to_image(base64_code):
-    # base64解码
-    img_data = base64.b64decode(base64_code)
-    # 转换为np数组
-    img_array = np.fromstring(img_data, np.uint8)
-    # 转换成opencv可用格式
-    img = cv.imdecode(img_array, cv.COLOR_RGB2BGR)
-    return img
-
-
-
+d_queue = SendFrame(maxlen=100)
 def format_img(np_img):
 
     _H, _W, _ = np_img.shape
@@ -61,8 +49,8 @@ def format_img(np_img):
     return im, dif_w, dif_h, factor
 
 # 加载模型
-def load_model():
-    global bindings_addrs, context, context2
+def load_model_2d():
+    global bindings_addrs, context
     with open('2D_16.engine', 'rb') as f, trt.Runtime(logger) as runtime:
         model = runtime.deserialize_cuda_engine(f.read())
         for index in range(model.num_bindings):
@@ -74,6 +62,18 @@ def load_model():
         bindings_addrs = OrderedDict((n, d.ptr) for n, d in bindings.items())
         context = model.create_execution_context()
 
+def load_model_3d():
+    global bindings_addrs2, context2
+    with open('3D_16.engine', 'rb') as f, trt.Runtime(logger) as runtime:
+        model = runtime.deserialize_cuda_engine(f.read())
+        for index in range(model.num_bindings):
+            name = model.get_binding_name(index)
+            dtype = trt.nptype(model.get_binding_dtype(index))
+            shape = model.get_binding_shape(index)
+            data = torch.from_numpy(np.empty(shape, dtype=dtype)).to(device)
+            bindings2[name] = Binding(name, dtype, shape, data, int(data.data_ptr()))
+        bindings_addrs2 = OrderedDict((n, d.ptr) for n, d in bindings2.items())
+        context2 = model.create_execution_context()
 
 def process_output(img_src, output_2d):
     src_h, src_w, _ = img_src.shape
@@ -93,28 +93,12 @@ def image_to_base64(image_np):
     image_code = str(base64.b64encode(image))[2:-1]
     return image_code
 
-def event_stream2():
-    while True:
-        if len(d_queue) != 0:
-            dataType = d_queue.popleft()
-            _H, _W, _ = dataType.img.shape
-            img = cv.resize(dataType.img, (int(_W / 3), int(_H / 3)))
-            img_base64 = image_to_base64(img)
-            yield f"data: {json.dumps({'img_base64': img_base64, 'boxes': str(dataType.boxes / 3), 'id': str(dataType.class_id)})}\n\n"
-
-
 @app.route('/get_data')
 def stream2():
-    return Response(event_stream2(), mimetype="text/event-stream")
+    return Response(d_queue.frame(), mimetype="text/event-stream")
 
-
-i = 0
-@app.route('/process_img', methods=['POST', 'GET'])
-def process():
-    global i
-    data = request.json
-    img_src = base64_to_image(data['img_base64'])
-    img_blob, dif_w, dif_h, factor = format_img(img_src)
+def model_process_2D(img_src_2D):
+    img_blob, dif_w, dif_h, factor = format_img(img_src_2D)
     bindings_addrs['images'] = img_blob.data_ptr()
     context.execute_v2(list(bindings_addrs.values()))
     out_prob = bindings['output0'].data.squeeze()
@@ -126,16 +110,45 @@ def process():
     out_prob[:, 4] = value  # 写入confidence
     out_prob[:, 5] = idx
     out_prob = out_prob[:, :6].cpu().numpy()
-    boxes, class_ids = process_output(img_src, out_prob)
-    draw_on_src(img_src, boxes, class_ids)   # 可以开线程加入队列  节省处理时间
-    if len(d_queue) != d_queue.maxlen:
-        d_queue.append(DataType(img_src, boxes, class_ids))
-    # cv.imwrite(os.path.join('imgs', str(i) + '.jpg'), img_src)
-
+    return out_prob
+def model_process_3D(img_src_3D):
+    img_blob, dif_w, dif_h, factor = format_img(img_src_3D)
+    bindings_addrs2['images'] = img_blob.data_ptr()
+    context2.execute_v2(list(bindings_addrs2.values()))
+    out_prob = bindings2['output0'].data.squeeze()
+    out_prob = out_prob[out_prob[:, 4] > confidence]
+    out_prob[:, 0] -= out_prob[:, 2] / 2 + dif_w
+    out_prob[:, 1] -= out_prob[:, 3] / 2 + dif_h
+    out_prob[:, :4] *= factor
+    value, idx = torch.max(out_prob[:, 5:], dim=1)
+    out_prob[:, 4] = value  # 写入confidence
+    out_prob[:, 5] = idx
+    out_prob = out_prob[:, :6].cpu().numpy()
+    return out_prob
+i = 0
+def img_save(img_src):
+    global i
+    cv.imwrite(os.path.join('imgs', str(i) + '.jpg'), img_src)
     i += 1
+@app.route('/process_img', methods=['POST', 'GET'])
+def process():
+    t1 = time.time()
+    data = request.json
+    img_src_2D = base64_to_image(data['img_base64_2D'])
+    img_src_3D = base64_to_image(data['img_base64_3D'])
+    out_prob_2D = model_process_2D(img_src_2D)
+    out_prob_3D = model_process_3D(img_src_3D)
+    out_prob = np.concatenate([out_prob_2D, out_prob_3D], axis=0)
+    boxes, class_ids = process_output(img_src_2D, out_prob)
+    draw_on_src(img_src_2D, boxes, class_ids)   # 可以开线程加入队列  节省处理时间
+    d_queue.append(DataType(img_src_2D, boxes, class_ids))
+    print(1 / (time.time() - t1))
+    threading.Thread(target=img_save, args=(img_src_2D,)).start()
     return jsonify({'ret': '处理完毕, 准备下一批的处理'})
 
-load_model()
+load_model_2d()
+load_model_3d()
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
 
